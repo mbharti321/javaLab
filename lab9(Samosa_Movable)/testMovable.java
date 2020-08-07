d6e3ee1e5e07aa46f35938f71a80331eee6b0d9c
Manish Bharti <mbharti321@gmail.com>
2020-08-07 21:29:22 +0530

    Update testMovable.java

--- lab9(Samosa_Movable)/testMovable.java
+++ lab9(Samosa_Movable)/testMovable.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 interface Movable
 {
-	public void moveup();
+	public void moveUp();
 	public void moveDown();
 	public void moveLeft();
 	public void moveRight();
@@ -11,96 +11,94 @@ interface Movable
 class MovablePoint implements Movable
 {
 	public int x,y,xSpeed,ySpeed;
-	public void MovablePoint(int x, int y, int xSpeed, int ySpeed)
+	MovablePoint(int x, int y, int xSpeed, int ySpeed)
 	{
 		this.x = x;
 		this.y = y;
 		this.xSpeed = xSpeed;
 		this.ySpeed = ySpeed;
 	}
-	
-	
-	
+		
 	public void moveUp()
 	{
-		y -= ySpeed;
+		this.y -= ySpeed;
 	}
-	
-		public void moveDown()
+						
+	public void moveDown()
 	{
-		y += ySpeed;
+		this.y += ySpeed;
 	}
-	
-		public void moveRight()
+			
+	public void moveRight()
 	{
-		x += xSpeed;
+		this.x += xSpeed;
 	}
-	
-		public void moveLeft()
+			
+	public void moveLeft()
 	{
-		x -= xSpeed;
+		this.x -= xSpeed;
 	}
-	
-		public String toString()
-		{
-			return "[Position] x : " + x + " y : " + y + "\n"
-		       + "[Speed] x : " + xSpeed +" y : " + ySpeed;
-		}
-	
+							
+	public String toString()
+	{
+		return "[Position] x : " + x + " y : " + y + "\n"
+	       + "[Speed] x : " + xSpeed +" y : " + ySpeed;
+	}
+																											
 }
 
-   class MovableCircle implements Movable
-	{
+class MovableCircle implements Movable
+{
 		private int radius;
 		private MovablePoint center;
 		
-		public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius)
+		MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius)
 		{
 			this.radius = radius;
 			this.center = new MovablePoint(x,y,xSpeed,ySpeed);
 		}
-		
-		  void moveUp()
+						
+		public void moveUp()
 		{
 			center.moveUp();
 		}
-		
-	       void moveDown()
+											
+		public void moveDown()
 		{
 			center.moveDown();
 		}
 		
-		  void moveRight()
+		public void moveRight()
 		{
 			center.moveRight();
 		}
-		
-		 void moveLeft()
+					
+		public void moveLeft()
 		{
 			center.moveLeft();
 		}
-		
+				
 		public String toString()
 		{
 			return "[Position] x : " + center.x + " y : " + center.y + "\n"
-		       + "[Speed] x : " + center.xSpeed +" y : " + center.ySpeed;
+										       + "[Speed] x : " + center.xSpeed +" y : " + center.ySpeed;
 		}
-	}
-	
-	public class test 
+}
+		
+public class test 
+{
+	public static void main(String[] args)
 	{
-   	public static void main(String[] args)
-   	{
-   	  Movable m1 = new MovablePoint(5,5,6,10);
-   	  System.out.println(m1);
-   	  m1.moveLeft();
-   	  System.out.println(m1);
-   	  
-   	  Movable m2 = new MovableCircle(2,1,2,-3,20);
-   	  System.out.println(m2);
-   	  m2.moveRight();
-   	  System.out.println(m2);
-   	  
-	}
+		MovablePoint m1 = new MovablePoint(5,5,6,10);
+		System.out.println(m1);
+		m1.moveLeft();
+		System.out.println(m1);
+		
+		MovableCircle m2 = new MovableCircle(2,1,2,-3,20);
+		System.out.println(m2);
+		m2.moveRight();
+		System.out.println(m2);
 
+	}
+		
 }
