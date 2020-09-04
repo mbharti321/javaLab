7bbe653a046f6ccec8ca28585c290088dab6f21b
Manish Bharti <mbharti321@gmail.com>
2020-09-04 14:49:55 +0530

    Update TestArrayList.java

--- lab_(genericArrayList)/TestArrayList.java
+++ lab_(genericArrayList)/TestArrayList.java
@@ -1,9 +1,10 @@
+/**************************************************************************************
 Write a class called TestArrayList which will have the following.
-
-Create an Array list of Strings, write method
-
-·     “Switch” which will switch alternate words in the array list. If the size of the array list is odd leave the last element in it’s original position.
-
-·     “Hide” which will hide the characters of a string whose length is divisible by 2 by “#”
-
-Create an array list of integers and write a method “removeunfairnumber” which will remove a number if it is less than the number to its left.
+ Create an Array list of Strings, write method
+    · “Switch” which will switch alternate words in the array list. If the size of the 
+         array list is odd leave the last element in it’s original position.
+    · “Hide” which will hide the characters of a string whose length is divisible by 2 by “#”
+    
+Create an array list of integers and write a method “removeunfairnumber” which will remove 
+a number if it is less than the number to its left.
+*****************************************************************************************/
